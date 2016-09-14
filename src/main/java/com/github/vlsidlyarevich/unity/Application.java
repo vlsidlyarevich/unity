585a2c7ca7f7e29d192cea2b0a0a4a3ab1fef1fc
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-14 20:36:48 +0300

    master: initial commit

---
+++ src/main/java/com/github/vlsidlyarevich/unity/Application.java
@@ -0,0 +1,20 @@
+package com.github.vlsidlyarevich.unity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by vlad on 14.09.16.
+ */
+@ComponentScan
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/vlsidlyarevich/unity/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package com.github.vlsidlyarevich.unity.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vlad on 14.09.16.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public ResponseEntity sayHello() {
+        return new ResponseEntity("Hello!", HttpStatus.OK);
+    }
+
+}
