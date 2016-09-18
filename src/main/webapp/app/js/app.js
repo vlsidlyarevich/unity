3ca5aa92450dbf4a6cd8abf2ffefb9e884383c17
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-18 12:51:03 +0300

    #5 - workers controller

---
+++ src/main/webapp/app/js/app.js
@@ -0,0 +1,19 @@
+var unityApp = angular.module('unityApp', []);
+
+unityApp.controller('WorkersListController', function WorkersListController($scope) {
+    $scope.workers = [
+        {
+            firstName: 'Vladislav',
+            lastName: 'Sidlyarevich',
+            age: '19'
+        }, {
+            firstName: 'Nikita',
+            lastName: 'Krivitski',
+            age: '20'
+        }, {
+            firstName: 'Karina',
+            lastName: 'Denisevich',
+            age: '19'
+        }
+    ];
+});
