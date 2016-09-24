e12992b9c113123d8a4946f44dd510d775188113
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-24 12:00:08 +0300

    master: added middlename field to name model

--- src/main/java/com/github/vlsidlyarevich/unity/models/Name.java
+++ src/main/java/com/github/vlsidlyarevich/unity/models/Name.java
@@ -13,15 +13,20 @@ import lombok.ToString;
 public class Name {
 
     private String firstName;
-
+    private String middleName;
     private String lastName;
 
     public Name() {
-
     }
 
     public Name(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Name(String firstName, String lastName, String middleName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
     }
 }
