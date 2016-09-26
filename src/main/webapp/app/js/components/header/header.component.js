2aa1db789513ba7a5a1339d7abf9da8f7d77abd8
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-26 20:42:13 +0300

    #6 - angularjs base Header component added

---
+++ src/main/webapp/app/js/components/header/header.component.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function HeaderController() {
+
+}
+
+angular.module('unityHeader').component('unityHeader', {
+    templateUrl: "js/components/header/header.component.html",
+    controller: HeaderController
+});
+
