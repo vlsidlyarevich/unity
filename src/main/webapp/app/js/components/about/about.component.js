d1c291f52087fa08b56b9bc1e500a654467943d6
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-26 22:24:28 +0300

    #13 - ui:about component

---
+++ src/main/webapp/app/js/components/about/about.component.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function AboutController() {
+
+}
+
+angular.module('about').component('about', {
+    templateUrl: "js/components/about/about.component.html",
+    controller: AboutController
+});
+
---
+++ src/main/webapp/app/js/components/about/about.component.module.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Define the `about` module
+angular.module('about', []);
