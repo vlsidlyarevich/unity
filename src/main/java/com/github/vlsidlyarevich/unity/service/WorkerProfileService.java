cac1befb4b850a6d08d4efdd86e42ba9540acfa5
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-29 17:34:30 +0300

    #14 - ui:new worker profile component New service for worker profile

---
+++ src/main/java/com/github/vlsidlyarevich/unity/service/WorkerProfileService.java
@@ -0,0 +1,38 @@
+package com.github.vlsidlyarevich.unity.service;
+
+import com.github.vlsidlyarevich.unity.models.Name;
+import com.github.vlsidlyarevich.unity.models.Speciality;
+import com.github.vlsidlyarevich.unity.models.Worker;
+import com.github.vlsidlyarevich.unity.models.WorkerProfile;
+import com.github.vlsidlyarevich.unity.repository.WorkerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by vlad on 28.09.16.
+ */
+@Service
+public class WorkerProfileService {
+
+    @Autowired
+    private WorkerProfileRepository repository;
+
+    public WorkerProfile findByName(Name name) {
+        return repository.findByName(name);
+    }
+
+    public List<WorkerProfile> findAllByAge(Integer age) {
+        return repository.findAllByAge(age);
+    }
+
+    public List<WorkerProfile> findAll() {
+        return repository.findAll();
+    }
+
+    public List<WorkerProfile> findAllBySpeciality(Speciality speciality) {
+        return repository.findAllBySpeciality(speciality);
+    }
+
+}
