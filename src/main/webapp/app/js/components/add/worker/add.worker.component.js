828039af762fa730b7053e89cac281189a4aa28f
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-09-29 17:33:48 +0300

    #14 - ui:new worker profile component Added prototype

---
+++ src/main/webapp/app/js/components/add/worker/add.worker.component.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function AddWorkerController($scope) {
+
+}
+
+angular.module('addWorker').component('addWorker', {
+    templateUrl: "js/components/add/worker/add.worker.component.html",
+    controller: AddWorkerController
+});
+
