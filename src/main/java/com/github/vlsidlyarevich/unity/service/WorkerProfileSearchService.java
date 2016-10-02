2467a64a83cb2ba797c32909abdc73bb5f33a74a
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-02 20:43:19 +0300

    Implemented searching worker profile by criteria

--- src/main/java/com/github/vlsidlyarevich/unity/service/WorkerProfileSearchService.java
+++ src/main/java/com/github/vlsidlyarevich/unity/service/WorkerProfileSearchService.java
@@ -9,7 +9,9 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vlad on 02.10.16.
@@ -26,7 +28,44 @@ public class WorkerProfileSearchService {
     public List<WorkerProfile> findByFilters(MultiValueMap<String, String> filters) {
         Query query = new Query();
 
+        for (Map.Entry<String, String> filter : prepareParameters(filters).entrySet()) {
+            switch (filter.getKey()) {
+                case "firstname": {
+                    query.addCriteria(Criteria.where("name.firstName").is(filter.getValue()));
+                    break;
+                }
+                case "secondname": {
+                    query.addCriteria(Criteria.where("name.secondName").is(filter.getValue()));
+                    break;
+                }
+                case "age": {
+                    query.addCriteria(Criteria.where("age").is(filter.getValue()));
+                    break;
+                }
+                case "gender": {
+                    query.addCriteria(Criteria.where("gender").is(filter.getValue()));
+                    break;
+                }
+                case "speciality": {
+                    query.addCriteria(Criteria.where("speciality").is(filter.getValue()));
+                    break;
+                }
+                case "skype": {
+                    query.addCriteria(Criteria.where("skype").is(filter.getValue()));
+                    break;
+                }
+            }
+        }
         return mongoOperations.find(query, WorkerProfile.class);
+    }
+
+    private Map<String, String> prepareParameters(MultiValueMap<String, String> filters) {
+        Map<String, String> parameters = new HashMap<String, String>();
+
+        for (String theKey : filters.keySet()) {
+            parameters.put(theKey, filters.getFirst(theKey));
+        }
+        return parameters;
     }
 
 }
