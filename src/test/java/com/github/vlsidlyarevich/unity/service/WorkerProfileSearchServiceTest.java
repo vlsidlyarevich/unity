00e4cca6e05aee763217c4efeb67f3f750c469b3
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-02 20:39:21 +0300

    Added test utils and worker profile search service test

--- src/test/java/com/github/vlsidlyarevich/unity/service/WorkerProfileSearchServiceTest.java
+++ src/test/java/com/github/vlsidlyarevich/unity/service/WorkerProfileSearchServiceTest.java
@@ -1,7 +1,10 @@
 package com.github.vlsidlyarevich.unity.service;
 
 import com.github.vlsidlyarevich.unity.Application;
+import com.github.vlsidlyarevich.unity.models.WorkerProfile;
 import com.github.vlsidlyarevich.unity.repository.WorkerProfileRepository;
+import com.github.vlsidlyarevich.unity.utils.TestUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
 
 /**
  * Created by vlad on 02.10.16.
@@ -21,9 +28,18 @@ public class WorkerProfileSearchServiceTest {
     @Autowired
     private WorkerProfileSearchService workerProfileSearchService;
 
-    @Test
-    public void findByFiltersTest(){
-        workerProfileSearchService.findByFilters(null);
-    }
+    @Autowired
+    private WorkerProfileService workerProfileService;
 
+    @Test
+    public void findByFiltersTest() {
+        WorkerProfile workerProfile = TestUtils.generateUserProfile();
+        workerProfileService.save(workerProfile);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("firstname", workerProfile.getName().getFirstName());
+        map.add("skype", workerProfile.getSkype());
+        List<WorkerProfile> workerProfiles = workerProfileSearchService.findByFilters(map);
+        Assert.assertEquals(workerProfile, workerProfiles.get(0));
+    }
 }
---
+++ src/test/java/com/github/vlsidlyarevich/unity/utils/TestUtils.java
@@ -0,0 +1,43 @@
+package com.github.vlsidlyarevich.unity.utils;
+
+import com.github.vlsidlyarevich.unity.models.Gender;
+import com.github.vlsidlyarevich.unity.models.Name;
+import com.github.vlsidlyarevich.unity.models.Speciality;
+import com.github.vlsidlyarevich.unity.models.WorkerProfile;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+
+/**
+ * Created by vlad on 02.10.16.
+ */
+public final class TestUtils {
+
+    private static final int MAX_STRING_LENGTH = 15;
+    private static final int START_INCLUSIVE = 5000;
+    private static final int END_INCLUSIVE = 100000;
+
+    private TestUtils() {
+
+    }
+
+    private static String getRandomString(int length) {
+        return RandomStringUtils.random(length, true, true);
+    }
+
+    private static Integer getRandomInt(int startInclusive, int endInclusive) {
+        return RandomUtils.nextInt(startInclusive, endInclusive);
+    }
+
+    public static WorkerProfile generateUserProfile() {
+        WorkerProfile workerProfile = new WorkerProfile();
+        workerProfile.setName(new Name(getRandomString(MAX_STRING_LENGTH), getRandomString(MAX_STRING_LENGTH)));
+        workerProfile.setSpeciality(Speciality.SOFTWARE_ENGINEER);
+        workerProfile.setAge(getRandomInt(18, 50));
+        workerProfile.setSkype(getRandomString(MAX_STRING_LENGTH));
+        workerProfile.setGender(Gender.MALE);
+        workerProfile.setPhone("8 800 555 35 35");
+
+        return workerProfile;
+    }
+
+}
