93bd200641267abe2ddf9047e13a2652932a0e3f
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-03 20:43:59 +0300

    Switch to map as request body

--- src/main/java/com/github/vlsidlyarevich/unity/controller/WorkerProfileSearchController.java
+++ src/main/java/com/github/vlsidlyarevich/unity/controller/WorkerProfileSearchController.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by vlad on 30.09.16.
@@ -20,7 +23,7 @@ public class WorkerProfileSearchController {
     private WorkerProfileSearchService service;
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> getWorkersByFilters(MultiValueMap<String, String> filters) {
+    public ResponseEntity<?> getWorkersByFilters(@RequestBody Map<String, String> filters) {
         return new ResponseEntity<>(service.findByFilters(filters), HttpStatus.OK);
     }
 
