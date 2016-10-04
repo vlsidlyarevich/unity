c52c0c8bcb2779cc1f5e89684e731d04b502e6fa
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-04 21:00:34 +0300

    Clean up

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,27 +1,11 @@
 "use strict";
 
-var unityApp = angular.module('unityApp', [
+var unityApp = angular.module('unity', [
     'ngRoute',
     'aboutComponent',
     'unityFooter',
     'unityHeader',
+    'workersGrid',
+    'addWorker',
     'ngAnimate'
 ]);
-
-unityApp.controller('WorkersListController', function WorkersListController($scope) {
-    $scope.workers = [
-        {
-            firstName: 'Vladislav',
-            lastName: 'Sidlyarevich',
-            age: '19'
-        }, {
-            firstName: 'Nikita',
-            lastName: 'Krivitski',
-            age: '20'
-        }, {
-            firstName: 'Karina',
-            lastName: 'Denisevich',
-            age: '19'
-        }
-    ];
-});
