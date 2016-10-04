012afc3f1eff4f94da05f6f9b8382925007b2c9b
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-04 21:00:24 +0300

    #14 - ui:new worker profile component

--- src/main/webapp/app/js/components/add/worker/add.worker.component.js
+++ src/main/webapp/app/js/components/add/worker/add.worker.component.js
@@ -1,7 +1,19 @@
 "use strict";
 
-function AddWorkerController($scope) {
+function AddWorkerController($scope, $http, $location, WorkerService) {
+    $scope.worker = $scope.worker || {};
 
+    $scope.submit = function (worker) {
+        console.log(worker);
+        WorkerService.saveWorker($scope, $http, worker, function (result) {
+            console.log(result);
+        });
+        $location.path('/workers');
+    };
+
+    $scope.close = function () {
+        $location.path('/workers')
+    }
 }
 
 angular.module('addWorker').component('addWorker', {
