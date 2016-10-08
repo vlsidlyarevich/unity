8d63e1ae06c96a239f37d0053bb76e8740519314
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-08 19:04:38 +0300

    Added util class for converting dto's to domain objects

---
+++ src/main/java/com/github/vlsidlyarevich/unity/utils/ModelUtils.java
@@ -0,0 +1,32 @@
+package com.github.vlsidlyarevich.unity.utils;
+
+import com.github.vlsidlyarevich.unity.dto.WorkerProfileDTO;
+import com.github.vlsidlyarevich.unity.model.Name;
+import com.github.vlsidlyarevich.unity.model.WorkerProfile;
+
+/**
+ * Created by vladislav on 10/8/16.
+ */
+public final class ModelUtils {
+
+    private ModelUtils() {
+    }
+
+    public static WorkerProfile convertToModelProfile(WorkerProfileDTO dto) {
+        WorkerProfile workerProfile = new WorkerProfile();
+        workerProfile.setName(dto.getName() != null ? dto.getName() : (new Name()));
+        workerProfile.setAge(dto.getAge());
+        workerProfile.setEmail(dto.getEmail() != null ? dto.getEmail() : "");
+        workerProfile.setPhone(dto.getPhone() != null ? dto.getPhone() : "");
+        workerProfile.setGender(dto.getGender());
+        workerProfile.setSpeciality(dto.getSpeciality());
+        workerProfile.setImageUrl(dto.getImageUrl());
+        workerProfile.setDescription(dto.getDescription() != null ? dto.getDescription() : "");
+        workerProfile.setBirthday(dto.getBirthday() != null ? dto.getBirthday() : "");
+        workerProfile.setSkype(dto.getSkype() != null ? dto.getSkype() : "");
+        workerProfile.setGithubUrl(dto.getGithubUrl() != null ? dto.getGithubUrl() : "");
+        workerProfile.setLinkedInUrl(dto.getLinkedInUrl() != null ? dto.getLinkedInUrl() : "");
+
+        return workerProfile;
+    }
+}
