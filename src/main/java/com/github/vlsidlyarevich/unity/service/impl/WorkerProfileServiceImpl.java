396027bf35c408627f5d0c32384ecda2341389fa
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-08 13:07:35 +0300

    Services moved to impl package

---
+++ src/main/java/com/github/vlsidlyarevich/unity/service/impl/WorkerProfileServiceImpl.java
@@ -0,0 +1,55 @@
+package com.github.vlsidlyarevich.unity.service.impl;
+
+import com.github.vlsidlyarevich.unity.models.Name;
+import com.github.vlsidlyarevich.unity.models.WorkerProfile;
+import com.github.vlsidlyarevich.unity.repository.WorkerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by vlad on 28.09.16.
+ */
+@Service
+public class WorkerProfileServiceImpl {
+
+    @Autowired
+    private WorkerProfileRepository repository;
+
+    public void save(WorkerProfile workerProfile) {
+        workerProfile.setCreatedAt(String.valueOf(LocalDateTime.now()));
+        repository.save(workerProfile);
+    }
+
+    public WorkerProfile findById(String id) {
+        return repository.findById(id);
+    }
+
+    public WorkerProfile findByName(Name name) {
+        return repository.findByName(name);
+    }
+
+    public List<WorkerProfile> findAllByAge(Integer age) {
+        return repository.findAllByAge(age);
+    }
+
+    public List<WorkerProfile> findAll() {
+        return repository.findAll();
+    }
+
+    public void updateWorkerProfileById(WorkerProfile workerProfile) {
+        if (repository.exists(workerProfile.getId())) {
+            workerProfile.setCreatedAt(repository.findById(workerProfile.getId()).getCreatedAt());
+            workerProfile.setUpdatedAt(String.valueOf(LocalDateTime.now()));
+        } else {
+            workerProfile.setCreatedAt(String.valueOf(LocalDateTime.now()));
+        }
+        repository.save(workerProfile);
+    }
+
+    public void deleteWorkerProfileById(String id) {
+        repository.delete(id);
+    }
+}
