d0754b5007c85bb48e7a3f5e7e4a90c309e5fdfe
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-10-26 23:42:20 +0300

    #10 - ui:workers grid component

--- src/main/webapp/app/js/components/workers/workers.component.js
+++ src/main/webapp/app/js/components/workers/workers.component.js
@@ -1,7 +1,19 @@
 "use strict";
 
-function WorkersController($scope, $http, WorkerService) {
-    WorkerService.getWorkers($scope, $http);
+function WorkersController($scope, $http, WorkerService, ImageService) {
+    $scope.workers = $scope.workers || {};
+
+    $scope.getWorkers = function () {
+        WorkerService.getWorkers($scope, $http, function (response) {
+            $scope.workers = response.data;
+        });
+    };
+
+    $scope.getWorkerImage = function (id) {
+        return ImageService.getImage($http, id, function (response) {
+            return response.body;
+        });
+    }
 }
 
 angular.module('workersGrid').component('workersGrid', {
