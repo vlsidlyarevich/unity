188866c84813f1c46b2180448073fa73c56a7b14
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-08 20:12:41 +0300

    #23 - ui: worker details component Added worker details component

---
+++ src/main/webapp/app/js/components/workers/details/worker.details.component.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function WorkerDetailsController($scope, $location, $routeParams, Worker, Image) {
+
+    $scope.worker = Worker.get({id: $routeParams.workerId});
+
+    $scope.submit = function () {
+        console.log("Saving: " + this.worker);
+        Worker.update({id: $routeParams.workerId}, this.worker).$promise.then(
+            function () {
+                $location.path('/workers');
+            }
+        );
+    };
+
+    $scope.fileUploaded = function ($file, $message) {
+        $scope.worker.imageId = $message;
+    };
+
+    $scope.resetImage = function ($flow) {
+        console.log($scope.worker.imageId);
+        Image.delete({id: $scope.worker.imageId}, function () {
+            console.log("Reset image with id: ".concat(id));
+        });
+
+        $flow.cancel();
+    };
+
+    $scope.close = function () {
+        $location.path('/workers')
+    }
+}
+
+angular.module('workerDetails').component('workerDetails', {
+    templateUrl: "app/js/components/workers/add/add.worker.component.html",
+    controller: WorkerDetailsController
+});
