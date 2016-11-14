9edc1ae1cee2e322e1afd3601b93159ebde1220c
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-14 23:48:01 +0300

    #29 - backend:vacancy logic Added Vacancy type and JobType enum

---
+++ src/main/java/com/github/vlsidlyarevich/unity/model/JobType.java
@@ -0,0 +1,15 @@
+package com.github.vlsidlyarevich.unity.model;
+
+import lombok.ToString;
+
+/**
+ * Created by vlad on 14/11/16.
+ */
+@ToString
+public enum JobType {
+    CONTRACT,
+    PERMANENT;
+
+    JobType() {
+    }
+}
---
+++ src/main/java/com/github/vlsidlyarevich/unity/model/Vacancy.java
@@ -0,0 +1,60 @@
+package com.github.vlsidlyarevich.unity.model;
+
+import lombok.Data;
+import lombok.ToString;
+import org.apache.commons.lang3.EnumUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by vlad on 14/11/16.
+ */
+@Data
+@ToString
+@Component
+public class Vacancy extends BaseEntity {
+
+    private Speciality speciality;
+    private JobType jobType;
+    private String lastUpdateDate;
+    private String datePosted;
+    private String location;
+    private String salary;
+    private String description;
+
+    @PostConstruct
+    public void init() {
+        this.jobType = JobType.CONTRACT;
+        this.speciality = Speciality.UNKNOWN;
+    }
+
+    public Vacancy(Speciality speciality) {
+        this.speciality = speciality;
+    }
+
+    public Vacancy(Speciality speciality, JobType jobType) {
+        this.speciality = speciality;
+        this.jobType = jobType;
+    }
+
+    public void setJobType(String jobType) {
+        if (EnumUtils.isValidEnum(JobType.class, jobType)) {
+            this.speciality = Speciality.valueOf(jobType);
+        }
+    }
+
+    public void setJobType(JobType jobType) {
+        this.jobType = jobType;
+    }
+
+    public void setSpeciality(String speciality) {
+        if (EnumUtils.isValidEnum(Speciality.class, speciality)) {
+            this.speciality = Speciality.valueOf(speciality);
+        }
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
+}
