1c165d2afb100bcb887a766d783d25918e649f91
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-15 11:28:26 +0300

    Added necessary configuration for Neo4J

--- src/main/java/com/github/vlsidlyarevich/unity/Application.java
+++ src/main/java/com/github/vlsidlyarevich/unity/Application.java
@@ -2,20 +2,16 @@ package com.github.vlsidlyarevich.unity;
 
 import com.github.vlsidlyarevich.unity.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 /**
  * Created by vlad on 14.09.16.
  */
-@ComponentScan
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 
---
+++ src/main/java/com/github/vlsidlyarevich/unity/config/Neo4jConfig.java
@@ -0,0 +1,40 @@
+package com.github.vlsidlyarevich.unity.config;
+
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by vlad on 15/11/16.
+ */
+@Configuration
+@EnableNeo4jRepositories({"com.github.vlsidlyarevich.unity.repository"})
+@EnableTransactionManagement
+public class Neo4jConfig extends Neo4jConfiguration {
+
+    @Value("${spring.data.neo4j.uri}")
+    private String URI;
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration();
+
+        configuration
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
+                .setURI(URI);
+
+        return configuration;
+    }
+
+    @Override
+    public SessionFactory getSessionFactory() {
+        return new SessionFactory(getConfiguration(),
+                "com.github.vlsidlyarevich.unity.model"
+        );
+    }
+}
