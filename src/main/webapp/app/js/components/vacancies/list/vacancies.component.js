29eaaa512c11c4c0bbc81814042931a0ba6728ac
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-23 15:42:02 +0300

    #28 - ui: vacansies logic Added delete logic to vacancies list

--- src/main/webapp/app/js/components/vacancies/list/vacancies.component.js
+++ src/main/webapp/app/js/components/vacancies/list/vacancies.component.js
@@ -1,16 +1,66 @@
 "use strict";
 
-function VacanciesController($scope, Vacancy, $route, ngDialog) {
+function VacanciesController($scope, Vacancy, VacancyDelete, $route, ngDialog) {
     $scope.vacancies = $scope.vacancies || {};
-
-    $scope.editVacancy = function ($index) {
-        ngDialog.open({
-            template:'app/js/components/vacancies/edit/vacancies.edit.modal.html'
-        });
-    };
 
     $scope.init = function () {
         $scope.vacancies = Vacancy.query();
+    };
+
+    $scope.editVacancy = function ($index) {
+        ngDialog.open({
+            template: 'app/js/components/vacancies/edit/vacancies.edit.modal.html'
+        });
+    };
+
+    $scope.deleteVacancy = function (id) {
+        Vacancy.delete({id: id}).$promise.then(
+            function () {
+                $route.reload();
+            }
+        )
+    };
+
+    $scope.deleteVacancies = function () {
+        var checked = this.vacancies.filter(function (item) {
+            return item.delete === true;
+        });
+
+        if (checked.length === this.vacancies.length) {
+            VacancyDelete.deleteQuery({"id": "all"}).$promise.then(
+                function () {
+                    $route.reload();
+                }
+            );
+        }
+        else {
+            var ids = {};
+            for (var i = 0; i < checked.length; i++) {
+                var id = "id".concat(i);
+                ids[id] = checked[i].id;
+            }
+
+            VacancyDelete.deleteQuery(ids).$promise.then(
+                function () {
+                    $route.reload();
+                }
+            );
+        }
+    };
+
+    $scope.selectAll = function () {
+        var value = true;
+
+        if (this.vacancies.filter(function (item) {
+                return item.delete === true;
+            }).length === this.vacancies.length) {
+
+            value = false;
+        }
+
+        for (var i = 0; i < this.vacancies.length; i++) {
+            this.vacancies[i].delete = value;
+        }
     };
 }
 
