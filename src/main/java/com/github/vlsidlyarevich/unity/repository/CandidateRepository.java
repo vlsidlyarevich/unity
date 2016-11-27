d94ee419bbd199e3ed3e7206ef2616759bc1f6c0
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-27 11:38:45 +0300

    #21 - backend:candidate logic

--- src/main/java/com/github/vlsidlyarevich/unity/repository/CandidateRepository.java
+++ src/main/java/com/github/vlsidlyarevich/unity/repository/CandidateRepository.java
@@ -1,7 +1,6 @@
 package com.github.vlsidlyarevich.unity.repository;
 
 import com.github.vlsidlyarevich.unity.model.Candidate;
-import com.github.vlsidlyarevich.unity.model.Vacancy;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,27 +8,14 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CandidateRepository extends GraphRepository<Vacancy> {
+public interface CandidateRepository extends GraphRepository<Candidate> {
 
-    @Query("MATCH (n) WHERE id(n)={0} RETURN n")
-    Vacancy findById(Long id);
-
-    @Query("MATCH (vacancy:Vacancy)" +
+    @Query("MATCH (vacancy:Vacancy)-[r:HAS_CANDIDATE]->(candidate:Candidate)-[n:HAS_NAME]->(name:Name)" +
             "WHERE id(vacancy)={vacancyId}" +
-            "CREATE (vacancy)-[:HAS_CANDIDATE]->(candidate:Candidate {hrSkype:{hrSkype}, skype:{skype}, githubUrl:{githubUrl}, " +
-            "linkedInUrl:{linkedInUrl}, imageId:{imageId}})" +
-            "RETURN candidate")
-    Candidate create(@Param("vacancyId") String vacancyId, @Param("hrSkype") String hrSkype,
-                     @Param("firstName") String firstName, @Param("secondName") String secondName,
-                     @Param("skype") String skype, @Param("githubUrl") String githubUrl,
-                     @Param("linkedInUrl") String linkedInUrl, @Param("imageId") String imageId);
+            "DELETE r,n,candidate,name")
+    void deleteAllInVacancy(@Param("vacancyId") Long vacancyId);
 
-    @Query("MATCH (vacancy:Vacancy)-[r:HAS_CANDIDATE]->(candidate:Candidate)" +
-            "WHERE id(vacancy)={vacancyId}" +
-            "DELETE candidate,r")
-    void deleteAllInVacancy(@Param("vacancyId") String vacancyId);
-
-    @Query("MATCH (candidate:Candidate)-[r:VACANCY]->(vacancy:Vacancy)" +
-            "DELETE candidate,r")
+    @Query("MATCH (candidate:Candidate)-[r:HAS_NAME]->(name:Name)" +
+            "DELETE candidate,r,name")
     void deleteAll();
 }
