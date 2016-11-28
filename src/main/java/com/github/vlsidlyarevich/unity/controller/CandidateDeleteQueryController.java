ad9ed6faa6471268999ac5a199bb6d2fe1150a56
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-11-28 23:45:12 +0300

    #21 - backend:candidate logic Added delete query controller for candidates

---
+++ src/main/java/com/github/vlsidlyarevich/unity/controller/CandidateDeleteQueryController.java
@@ -0,0 +1,26 @@
+package com.github.vlsidlyarevich.unity.controller;
+
+import com.github.vlsidlyarevich.unity.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/vacancies/{vacancyId}/candidates")
+public class CandidateDeleteQueryController {
+
+    @Autowired
+    private CandidateService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> deleteQuery(@RequestBody Map<String, String> ids) {
+        return new ResponseEntity<>(service.deleteQuery(ids), HttpStatus.OK);
+    }
+}
