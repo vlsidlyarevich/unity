0ecff1e38716f923bf5f4b071569d3f8129bff2a
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-20 00:00:42 +0300

    #41 - added class for security constants

---
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/constants/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.github.vlsidlyarevich.unity.auth.constants;
+
+
+public final class SecurityConstants {
+
+    public final static String AUTH_HEADER_NAME="x-auth-token";
+
+    private SecurityConstants(){
+
+    }
+}
