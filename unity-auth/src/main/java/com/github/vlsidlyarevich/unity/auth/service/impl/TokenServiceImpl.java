41b1f7c9904908155782da4fc242f9e7e9acb369
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-19 23:59:37 +0300

    #41 - added token service

--- unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenServiceImpl.java
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenServiceImpl.java
@@ -1,19 +1,53 @@
 package com.github.vlsidlyarevich.unity.auth.service.impl;
 
 import com.github.vlsidlyarevich.unity.auth.service.TokenService;
+import com.github.vlsidlyarevich.unity.db.model.User;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
 public class TokenServiceImpl implements TokenService {
+
+    @Value("security.token.secret.key")
+    private String tokenKey;
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
     public String getToken(String username, String password) {
-        return null;
+        if (username == null || password == null)
+            return null;
+        User user = (User) userDetailsService.loadUserByUsername(username);
+        Map<String, Object> tokenData = new HashMap<>();
+        if (password.equals(user.getPassword())) {
+            tokenData.put("clientType", "user");
+            tokenData.put("userID", user.getId());
+            tokenData.put("username", user.getUsername());
+            tokenData.put("token_create_date", LocalDateTime.now());
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.YEAR, 100);
+            tokenData.put("token_expiration_date", calendar.getTime());
+            JwtBuilder jwtBuilder = Jwts.builder();
+            jwtBuilder.setExpiration(calendar.getTime());
+            jwtBuilder.setClaims(tokenData);
+            return jwtBuilder.signWith(SignatureAlgorithm.HS512, tokenKey).compact();
+
+        } else {
+//            throw new Exception("Authentication error");
+            System.out.println("Error");
+            return null;
+        }
     }
 }
