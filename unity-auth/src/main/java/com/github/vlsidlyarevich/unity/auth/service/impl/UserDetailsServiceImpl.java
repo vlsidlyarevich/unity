2e4de17fdddd91fb7735499f8ce5b4d62f041eb3
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-20 00:01:25 +0300

    #41 - added dummy user details service

--- unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/UserDetailsServiceImpl.java
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.github.vlsidlyarevich.unity.auth.service.impl;
 
+import com.github.vlsidlyarevich.unity.db.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,8 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserService userService;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userService.findByUsername(username);
     }
 }
