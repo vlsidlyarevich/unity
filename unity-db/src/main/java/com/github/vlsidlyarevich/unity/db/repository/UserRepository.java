7a01be387d96ae4f450b8bc3779b9c5bead15f9d
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-19 23:59:20 +0300

    #41 - user repository extended

--- unity-db/src/main/java/com/github/vlsidlyarevich/unity/db/repository/UserRepository.java
+++ unity-db/src/main/java/com/github/vlsidlyarevich/unity/db/repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    User findByUsername(String userName);
 }
