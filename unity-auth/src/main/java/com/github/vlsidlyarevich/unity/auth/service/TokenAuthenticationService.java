e53b0e49c24c50e101816a726a579e52f84833d0
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-21 13:59:18 +0300

    #41 - token auth service implemented

--- unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/TokenAuthenticationService.java
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/TokenAuthenticationService.java
@@ -1,11 +1,15 @@
 package com.github.vlsidlyarevich.unity.auth.service;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 public interface TokenAuthenticationService {
 
     Authentication authenticate(HttpServletRequest request);
+
+    void addAuthentication(HttpServletResponse response, UserDetails userDetails);
 }
--- unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenAuthenticationServiceImpl.java
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenAuthenticationServiceImpl.java
@@ -1,17 +1,24 @@
 package com.github.vlsidlyarevich.unity.auth.service.impl;
 
+import com.github.vlsidlyarevich.unity.auth.constants.SecurityConstants;
+import com.github.vlsidlyarevich.unity.auth.exception.UserNotFoundException;
+import com.github.vlsidlyarevich.unity.auth.models.UserAuthentication;
 import com.github.vlsidlyarevich.unity.auth.service.TokenAuthenticationService;
+import com.github.vlsidlyarevich.unity.auth.service.TokenService;
+import com.github.vlsidlyarevich.unity.db.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.token.TokenService;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Service
@@ -23,12 +30,33 @@ public class TokenAuthenticationServiceImpl implements TokenAuthenticationServic
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public Authentication authenticate(HttpServletRequest request) {
-        final Jws<Claims> tokenData = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(request.getHeader(secretKey));
-        tokenData.getBody().getSubject()
-
+        String token = request.getHeader(secretKey);
+        if(token != null) {
+            final Jws<Claims> tokenData = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            User user = getUserFromToken(tokenData);
+            if(user!=null){
+                return new UserAuthentication(user);
+            }
+        }
+        return null;
     }
 
-    private User getUserFromToken(Jws<Claims> tokenData) throws UserNot
+    @Override
+    public void addAuthentication(HttpServletResponse response, UserDetails userDetails) {
+        final User user = (User) userDetails;
+        response.addHeader(SecurityConstants.AUTH_HEADER_NAME, tokenService.getToken(user.getUsername(), user.getPassword()));
+    }
+
+    private User getUserFromToken(Jws<Claims> tokenData) throws UserNotFoundException {
+        try {
+            return (User) userDetailsService.loadUserByUsername(tokenData.getBody().getSubject());
+        } catch (UsernameNotFoundException e) {
+            throw new UserNotFoundException("User " + tokenData.getBody().getSubject() + " not found");
+        }
+    }
 }
