96efb9598d0e1e26921519f7d3386ec4abca1f9e
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-21 13:58:50 +0300

    #41 - added user not found exception and user authentication

---
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/models/UserAuthentication.java
@@ -0,0 +1,53 @@
+package com.github.vlsidlyarevich.unity.auth.models;
+
+import com.github.vlsidlyarevich.unity.db.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+
+public class UserAuthentication implements Authentication{
+
+    private final User user;
+    private boolean authenticated = true;
+
+    public UserAuthentication(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Object getDetails() {
+        return user;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getName() {
+        return user.getUsername();
+    }
+}
