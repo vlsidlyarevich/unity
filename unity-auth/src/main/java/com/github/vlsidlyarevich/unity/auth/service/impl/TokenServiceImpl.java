18c0fb48fa12cc48b2bd49eb128aa9c5a123cba6
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-01-21 14:00:18 +0300

    #41 - fixed annotation in token service impl

--- unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenServiceImpl.java
+++ unity-auth/src/main/java/com/github/vlsidlyarevich/unity/auth/service/impl/TokenServiceImpl.java
@@ -8,7 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Calendar;
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@Component
+@Service
 public class TokenServiceImpl implements TokenService {
 
     @Value("security.token.secret.key")
