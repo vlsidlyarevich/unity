9eee79fa2778000666ae43caff537a080537e3a8
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-02-13 22:58:38 +0300

    #45 - simple signup form component

---
+++ unity-client/src/app/signup/components/signup-form/signup-form.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SignupFormComponent } from './signup-form.component';
+
+describe('SignupFormComponent', () => {
+  let component: SignupFormComponent;
+  let fixture: ComponentFixture<SignupFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SignupFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignupFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ unity-client/src/app/signup/components/signup-form/signup-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../../models/user";
+import { Router } from "@angular/router";
+import { SignupService } from "../../../services/SignupService";
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css']
+})
+export class SignupFormComponent implements OnInit {
+  signupModel: User = new User;
+  loading = false;
+  error = '';
+
+  constructor(private router: Router,
+              private signupService: SignupService) {
+  }
+
+  ngOnInit() {
+  }
+
+  signup() {
+    this.loading = true;
+    this.signupService.signup(this.signupModel)
+      .subscribe(result => {
+        if (result === true) {
+          this.router.navigate(['/auth']);
+        } else {
+          this.error = 'Unable to register a new user';
+          this.loading = false;
+        }
+      });
+  }
+}
