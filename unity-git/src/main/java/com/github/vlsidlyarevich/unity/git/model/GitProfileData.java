38d59908ae0e23d91e6a41d857bcb29d3faccfdb
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-03-22 21:29:33 +0300

    #48 - added models for git data

---
+++ unity-git/src/main/java/com/github/vlsidlyarevich/unity/git/model/GitProfileData.java
@@ -0,0 +1,36 @@
+package com.github.vlsidlyarevich.unity.git.model;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Data
+public class GitProfileData {
+
+    private Integer id;
+    private String login;
+    private String avatarUrl;
+    private String gravatarId;
+    private String url;
+    private String htmlUrl;
+    private String gistsUrl;
+    private String starredUrl;
+    private String subscriptionsUrl;
+    private String organizationsUrl;
+    private List<GitRepositoryData> repos;
+    private String type;
+    private String name;
+    private String company;
+    private String blog;
+    private String email;
+    private String hireable;
+    private String bio;
+    private Integer publicRepos;
+    private Integer publicGists;
+    private Integer followers;
+    private Integer following;
+    private Date createdAt;
+    private Date updatedAt;
+}
---
+++ unity-git/src/main/java/com/github/vlsidlyarevich/unity/git/model/GitRepositoryData.java
@@ -0,0 +1,7 @@
+package com.github.vlsidlyarevich.unity.git.model;
+
+/**
+ * Created by vlad on 22/03/17.
+ */
+public class GitRepositoryData {
+}
