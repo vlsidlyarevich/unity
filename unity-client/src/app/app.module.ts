9510a0550177893b0e16b5326718017f00a72c3b
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-03 23:42:41 +0300

    #51 - added header

--- unity-client/src/app/app.module.ts
+++ unity-client/src/app/app.module.ts
@@ -1,30 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
-import { AuthModule } from './auth/auth.module';
-import { SignupModule } from './signup/signup.module';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routes';
-import { AuthGuard } from './guards/auth.guard';
-import { AuthenticationService } from './services/AuthenticationService';
-import { SignupService } from './services/SignupService';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppComponent } from "./app.component";
+import { HomeModule } from "./home/home.module";
+import { AuthModule } from "./auth/auth.module";
+import { SignupModule } from "./signup/signup.module";
+import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app.routes";
+import { AuthGuard } from "./guards/auth.guard";
+import { AuthenticationService } from "./services/AuthenticationService";
+import { SignupService } from "./services/SignupService";
+import { HeaderModule } from "./header/header.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     ReactiveFormsModule,
     HttpModule,
     HomeModule,
     AuthModule,
     SignupModule,
+    HeaderModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     AuthGuard,
