536128ef5eb0a70ed611b400e3a1eceddebbb335
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-06 00:06:56 +0300

    #54 - implementation

--- unity-client/src/app/my-profile/my-profile.module.ts
+++ unity-client/src/app/my-profile/my-profile.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfileRoutingModule } from "./my-profile.routes";
 import { MyProfileComponent } from "./my-profile.component";
+import { MyProfileFormComponent } from './components/my-profile-form/my-profile-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MyProfileRoutingModule
   ],
-  declarations: [MyProfileComponent]
+  declarations: [MyProfileComponent, MyProfileFormComponent]
 })
 export class MyProfileModule {
 }
