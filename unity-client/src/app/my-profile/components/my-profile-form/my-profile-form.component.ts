f1f3378ead8b9cea781fde5900903957a98fa5ee
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-06 22:41:33 +0300

    #54 - my profile form implementation

--- unity-client/src/app/my-profile/components/my-profile-form/my-profile-form.component.ts
+++ unity-client/src/app/my-profile/components/my-profile-form/my-profile-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserData } from '../../../models/userData';
 import { ProfileService } from "../../../services/ProfileService";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-my-profile-form',
@@ -8,7 +9,7 @@ import { ProfileService } from "../../../services/ProfileService";
   styleUrls: ['./my-profile-form.component.css']
 })
 export class MyProfileFormComponent implements OnInit {
-  profile: UserData;
+  profile: FormGroup;
   loading = false;
   error = '';
 
@@ -19,7 +20,7 @@ export class MyProfileFormComponent implements OnInit {
     this.profileService.getUserData().subscribe(
       result => {
         if (result) {
-          this.profile = result;
+          this.fulfillForm(result);
         } else {
           this.error = "Unable to get profile data";
         }
@@ -29,7 +30,16 @@ export class MyProfileFormComponent implements OnInit {
       });
   }
 
-
-
-
+  private fulfillForm(userData: UserData): void {
+    this.profile = new FormGroup({
+      username: new FormControl(userData.username, [Validators.required, Validators.minLength(4)]),
+      password: new FormControl(JSON.parse(localStorage.getItem('currentUser')).password, [Validators.required,
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,}$')]),
+      firstName: new FormControl(userData.firstName),
+      lastName: new FormControl(userData.lastName),
+      email: new FormControl(userData.email),
+      skype: new FormControl(userData.skype),
+      additional: new FormControl(userData.additional)
+    });
+  }
 }
