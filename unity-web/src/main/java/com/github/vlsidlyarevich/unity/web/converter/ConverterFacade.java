43ba7926d5576b6baaff5160fe1fc4a1e506d22c
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-08 14:33:52 +0300

    Great clean up/removing old functionality

--- unity-web/src/main/java/com/github/vlsidlyarevich/unity/web/converter/ConverterFacade.java
+++ unity-web/src/main/java/com/github/vlsidlyarevich/unity/web/converter/ConverterFacade.java
@@ -1,14 +1,8 @@
 package com.github.vlsidlyarevich.unity.web.converter;
 
-import com.github.vlsidlyarevich.unity.db.model.Candidate;
 import com.github.vlsidlyarevich.unity.db.model.User;
-import com.github.vlsidlyarevich.unity.db.model.Vacancy;
-import com.github.vlsidlyarevich.unity.db.model.WorkerProfile;
 import com.github.vlsidlyarevich.unity.web.converter.factory.ConverterFactory;
-import com.github.vlsidlyarevich.unity.web.dto.CandidateDTO;
 import com.github.vlsidlyarevich.unity.web.dto.UserDTO;
-import com.github.vlsidlyarevich.unity.web.dto.VacancyDTO;
-import com.github.vlsidlyarevich.unity.web.dto.WorkerProfileDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,18 +11,6 @@ public class ConverterFacade {
 
     @Autowired
     private ConverterFactory converterFactory;
-
-    public WorkerProfile convert(WorkerProfileDTO dto) {
-        return (WorkerProfile) converterFactory.getConverter(dto.getClass()).convert(dto);
-    }
-
-    public Vacancy convert(VacancyDTO dto) {
-        return (Vacancy) converterFactory.getConverter(dto.getClass()).convert(dto);
-    }
-
-    public Candidate convert(CandidateDTO dto) {
-        return (Candidate) converterFactory.getConverter(dto.getClass()).convert(dto);
-    }
 
     public User convert(UserDTO dto){
         return (User) converterFactory.getConverter(dto.getClass()).convert(dto);
