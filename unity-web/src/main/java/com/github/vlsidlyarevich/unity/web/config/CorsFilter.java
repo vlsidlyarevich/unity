6f59c25569c47c8846af59590a7b6f818cb70312
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-17 00:27:43 +0300

    Fixed cors filter

--- unity-web/src/main/java/com/github/vlsidlyarevich/unity/web/config/CorsFilter.java
+++ unity-web/src/main/java/com/github/vlsidlyarevich/unity/web/config/CorsFilter.java
@@ -1,13 +1,17 @@
 package com.github.vlsidlyarevich.unity.web.config;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Priority;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
     @Override
