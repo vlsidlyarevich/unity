a857c5ca53f4958d7d85e41477228388750502ab
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-04-27 11:28:48 +0300

    Analyze source select on client implementation

--- unity-client/src/app/components/home/components/dashboard/dashboard.component.ts
+++ unity-client/src/app/components/home/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { DialogService } from "ng2-bootstrap-modal";
+import { AnalyzeSelectModalComponent } from "../analyze-select-modal/analyze-select-modal.component";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,31 @@ import { Component, OnInit } from "@angular/core";
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() {
+  constructor(private dialogService: DialogService) {
   }
 
   ngOnInit() {
+
+  }
+
+  showSelect() {
+    let disposable = this.dialogService.addDialog(AnalyzeSelectModalComponent, {
+      title: 'Confirm title',
+      message: 'Confirm message'
+    })
+      .subscribe((isConfirmed) => {
+        //We get dialog result
+        if (isConfirmed) {
+          alert('accepted');
+        }
+        else {
+          alert('declined');
+        }
+      });
+    //We can close dialog calling disposable.unsubscribe();
+    //If dialog was not closed manually close it by timeout
+    setTimeout(() => {
+      disposable.unsubscribe();
+    }, 1000000);
   }
 }
--- unity-client/src/app/components/home/home.module.ts
+++ unity-client/src/app/components/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routes';
+import { AnalyzeSelectModalComponent } from './components/analyze-select-modal/analyze-select-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [HomeComponent, DashboardComponent]
+  entryComponents: [
+    AnalyzeSelectModalComponent
+  ],
+  declarations: [HomeComponent, DashboardComponent, AnalyzeSelectModalComponent]
 })
 export class HomeModule {
 }
