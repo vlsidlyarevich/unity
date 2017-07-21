e88b6d06a6c323b082aa0968f6c29f67a0bcc821
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-07-21 22:51:35 +0300

    #75 - user analytics controller test implementation

--- src/integration-test/java/com/github/vlsidlyarevich/unity/web/controller/UserAnalyticsControllerIT.java
+++ src/integration-test/java/com/github/vlsidlyarevich/unity/web/controller/UserAnalyticsControllerIT.java
@@ -7,14 +7,18 @@ import com.github.vlsidlyarevich.unity.db.service.UserAnalyticsService;
 import com.github.vlsidlyarevich.unity.db.service.UserService;
 import com.github.vlsidlyarevich.unity.web.exception.handler.PersistanceExceptionHandler;
 import com.github.vlsidlyarevich.unity.web.exception.handler.SecurityExceptionHandler;
+import com.github.vlsidlyarevich.unity.web.security.constant.SecurityConstants;
 import com.github.vlsidlyarevich.unity.web.security.service.TokenService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.github.vlsidlyarevich.unity.TestUtils.createUserAnalytics;
@@ -54,39 +58,59 @@ public class UserAnalyticsControllerIT extends AbstractControllerIT {
     }
 
     @Test
-    public void getAnalyticsByUserId_Success_IfAnalyticsAndUserPresent() {
+    public void getAnalyticsByUserId_Success_IfAnalyticsAndUserPresent() throws Exception {
 
     }
 
-    public void getAnalyticsByUserId_BadRequest_IfUserNotPresent() {
+    public void getAnalyticsByUserId_BadRequest_IfUserNotPresent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/v1/user/id0/analytics/id0")
+                .accept(contentType)
+                .contentType(contentType)
+                .header(SecurityConstants.AUTH_HEADER_NAME, token))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    public void getAnalyticsReportById_Success_IfAnalyticsAndUserPresent() throws Exception {
 
     }
 
-    public void getAnalyticsReportById_Success_IfAnalyticsAndUserPresent() {
+    public void getAnalyticsReportById_NoContent_IfAnalyticsNotPresent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/v1/user/" + user.getId() + "/analytics/id0")
+                .accept(contentType)
+                .contentType(contentType)
+                .header(SecurityConstants.AUTH_HEADER_NAME, token))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    public void getAnalyticsReportById_BadRequest_IfUserNotPresent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/v1/user/id0")
+                .accept(contentType)
+                .contentType(contentType)
+                .header(SecurityConstants.AUTH_HEADER_NAME, token))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    public void deleteAnalyticsReportById_Success_IfAnalyticsAndUserPresent() throws Exception {
 
     }
 
-    public void getAnalyticsReportById_NoContent_IfAnalyticsNotPresent() {
+    public void deleteAnalyticsReportById_BadRequest_IfUserNotPresent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/v1/user/id0")
+                .accept(contentType)
+                .contentType(contentType)
+                .header(SecurityConstants.AUTH_HEADER_NAME, token))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    public void deleteAllAnalyticsReports_Success_IfUserPresent() throws Exception {
 
     }
 
-    public void getAnalyticsReportById_BadRequest_IfUserNotPresent() {
-
-    }
-
-    public void deleteAnalyticsReportById_Success_IfAnalyticsAndUserPresent() {
-
-    }
-
-    public void deleteAnalyticsReportById_BadRequest_IfUserNotPresent() {
-
-    }
-
-    public void deleteAllAnalyticsReports_Success_IfUserPresent() {
-
-    }
-
-    public void deleteAllAnalyticsReports_BadRequest_IfUserNotPresent() {
-
+    public void deleteAllAnalyticsReports_BadRequest_IfUserNotPresent() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/api/v1/user/id0")
+                .accept(contentType)
+                .contentType(contentType)
+                .header(SecurityConstants.AUTH_HEADER_NAME, token))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
