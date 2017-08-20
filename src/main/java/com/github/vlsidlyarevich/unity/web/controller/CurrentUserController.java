4ef1f1d6558e5e256885660b6fe543a57ecdebbd
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-08-20 20:25:46 +0300

    I just moved controllers to old package, just because i can

--- src/main/java/com/github/vlsidlyarevich/unity/web/controller/CurrentUserController.java
+++ src/main/java/com/github/vlsidlyarevich/unity/web/controller/CurrentUserController.java
@@ -1,4 +1,4 @@
-package com.github.vlsidlyarevich.unity.web.controller.api;
+package com.github.vlsidlyarevich.unity.web.controller;
 
 import com.github.vlsidlyarevich.unity.web.security.facade.AuthenticationFacade;
 import com.github.vlsidlyarevich.unity.domain.model.User;
