205121998071c3fc57db02a25d67d5a38d43dc73
vlsidlyarevich <vlsidlyarevich@gmail.com>
2017-08-28 19:09:17 +0300

    #90 - working for now)

--- src/main/java/com/github/vlsidlyarevich/unity/web/controller/LinkedinProfileAnalyticsController.java
+++ src/main/java/com/github/vlsidlyarevich/unity/web/controller/LinkedinProfileAnalyticsController.java
@@ -1,11 +1,12 @@
 package com.github.vlsidlyarevich.unity.web.controller;
 
 import com.github.vlsidlyarevich.unity.linkedin.service.LinkedInAnalyticsService;
+import com.github.vlsidlyarevich.unity.web.dto.LinkedInAnalyzeProfileRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,8 @@ public class LinkedinProfileAnalyticsController {
 
     private final LinkedInAnalyticsService service;
 
-    @RequestMapping(value = "/{userUrl}", method = RequestMethod.GET)
-    public ResponseEntity getLinkedInDataByUserUrl(@PathVariable final String userUrl) {
-        return ResponseEntity.ok(service.getLinkedInProfileAnalytics(userUrl));
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity getLinkedInDataByUserUrl(@RequestBody final LinkedInAnalyzeProfileRequest request) {
+        return ResponseEntity.ok(service.getLinkedInProfileAnalytics(request.getPublicUserUrl()));
     }
 }
---
+++ src/main/java/com/github/vlsidlyarevich/unity/web/dto/LinkedInAnalyzeProfileRequest.java
@@ -0,0 +1,13 @@
+package com.github.vlsidlyarevich.unity.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkedInAnalyzeProfileRequest {
+
+    private String publicUserUrl;
+}
