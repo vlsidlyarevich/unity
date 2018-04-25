c77368c0d76e4fad57c627d22cbe5f3aa72b386b
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2018-04-25 20:04:14 +0300

    Clean up repo (delete old client)

--- old-client/src/app/models/analyzeSource.ts
+++
@@ -1,5 +0,0 @@
-export enum AnalyzeSource {
-  Github,
-  Facebook,
-  Twitter
-}
--- old-client/src/app/models/credentials.ts
+++
@@ -1,9 +0,0 @@
-export class Credentials {
-  username: String;
-  password: String;
-
-  constructor(username: String, password: String) {
-    this.username = username;
-    this.password = password;
-  }
-}
